baf1dcaeae461d099246fb6b8e2bc3286a275108
gorkemyapici <gorkemyapici5@gmail.com>
2023-07-04 15:49:35 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gorkem YAPICI on 4.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
